0c1554cb9bee506d4ee89506a894c04450d763e7
Gabe Chavez <73260097+GabeCodes72@users.noreply.github.com>
2021-09-16 11:55:09 -0500

    Syntax Fixes
    Cut out excess code and imports. Improved clarity of what to do/type.

--- TAPC.py
+++ TAPC.py
@@ -1,5 +1,3 @@
-import math
-
 shifttime = int(input("Hours per Day > "))
 wage = int(input("Money per Hour > $"))
 income = shifttime*wage
@@ -16,13 +14,13 @@ year = round(ttg/365, 0)
 
 def aot():
   if 0 <= ttg <= 6:
-    print(f"It will take about {day} days to reach your goal")
+  		print(f"It will take about {day} days to reach your goal")
   elif 7 <= ttg <= 29:
-    print(f"It will take about {week} weeks to reach your goal.")
+  		print(f"It will take about {week} weeks to reach your goal.")
   elif 30 <= ttg <= 364:
-    print(f"It will take about {month} months to reach your goal.")
+    	print(f"It will take about {month} months to reach your goal.")
   elif ttg >= year:
-    print(f"It will take about {year} years to reach your goal.")
+    	print(f"It will take about {year} years to reach your goal.")
 print()
 aot()
 input("Thank you for using the TAP-C. Please press enter to exit the program.")
